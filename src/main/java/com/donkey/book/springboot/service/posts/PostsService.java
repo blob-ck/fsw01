ca60d330c0f6493ca45ce7e34eb0f8f269acc42b
blob-ck <hankwan777@gmail.com>
2022-05-28 16:32:26 +0900

    Post 등록 기능 추가 및 테스트

---
+++ src/main/java/com/donkey/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,17 @@
+package com.donkey.book.springboot.service.posts;
+
+import com.donkey.book.springboot.domain.posts.PostsRepository;
+import com.donkey.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
